768a1e96b8eb7fc5c640c48bf573c34bdb8d353d
Code Monad <code@lab-11.org>
2025-02-10 06:37:36 +0800

    fix initial message

--- src/bot.py
+++ src/bot.py
@@ -43,8 +43,9 @@ class VerificationBot(commands.Bot):
     async def get_guild_member(self, user_id: int):
         """Safe method to get guild member"""
         if not self.guild:
-            print(f"guild is None, please check your configuration")
             self.guild = self.get_guild(Config.TARGET_GUILD_ID)
+        if not self.guild:
+            print(f"Could not find guild with ID {Config.TARGET_GUILD_ID}")
         try:
             return await self.guild.fetch_member(user_id)
         except discord.NotFound:
@@ -57,11 +58,20 @@ class VerificationBot(commands.Bot):
     @tasks.loop(count=1)
     async def send_initial_message(self):
         channel = self.get_channel(Config.TARGET_CHANNEL_ID)
-        embed = discord.Embed(
-            title="Thanks for being a Nervape Holder!",
-            description="Click the button below to verify your holder status."
-        )
-        await channel.send(embed=embed, view=VerifyButton())
+        embed = discord.Embed(title="Thanks for being a Nervape Holder!", description="We use this verification bot to safely verify that you are a Nervape Holder and can receive the Nervape Holder Role in Discord. To get started, click the button below.\n\nBy verifying, you are agreeing to the Terms of Use and Privacy Policy of this verification process.")
+        embed.set_footer(text="Made by Nervape Studio with ❤️")
+        embed.set_author(name="Nervape Studio", icon_url="https://cdn.discordapp.com/icons/942680148212350996/84da741b575d3ac42a5bea3a67b57614.png")
+        res = await channel.send(embed=embed, view=VerifyButton(), silent=True)
+        last_initial_message = self.redis.get(f"{Config.REDIS_KEY_PREFIX}:discord:last_initial_message")
+        if last_initial_message:
+            last_initial_message = int(last_initial_message)
+            try:
+                message = await channel.fetch_message(last_initial_message)
+                await message.delete()
+            except discord.NotFound:
+                print(f"Initial message {last_initial_message} not found")
+                pass
+        self.redis.set(f"{Config.REDIS_KEY_PREFIX}:discord:last_initial_message", res.id)
 
     @tasks.loop(seconds=Config.CHECK_INTERVAL)
     async def check_addresses(self):
